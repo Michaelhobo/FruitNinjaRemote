69ff69cf765fc2a77ab96f86e970e875ed7d262c
Michael Ho <callmemikeh@gmail.com>
2012-10-12 23:07:12 -0700

    delete main, added acceleration tracking

--- src/AccelerometerDataActivity.java
+++ src/AccelerometerDataActivity.java
@@ -5,20 +5,33 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class AccelerometerDataActivity extends Activity implements
-		SensorEventListener {
-	int mLastX;
-	int mLastY;
-	int mLastZ;
-	boolean mInitialized;
+public class AccelerometerDataActivity extends Activity implements SensorEventListener {
 
+	float xLinearAcceleration;
+	float yLinearAcceleration;
+	float zLinearAcceleration;
+	float[] gravity = new float[3];
+	TextView display = new TextView(this);
+	public void onCreate()
+	{
+		setContentView(display);
+	}
+	
 	public void onAccuracyChanged(Sensor arg0, int arg1) {	}
 
 	public void onSensorChanged(SensorEvent event) 
 	{
-		
+		if (event.sensor != null && event.sensor.getType()==Sensor.TYPE_ACCELEROMETER)
+		{
+			
+			xLinearAcceleration=event.values[0];
+			yLinearAcceleration=event.values[1];
+			zLinearAcceleration=event.values[2];
+			display.setText(xLinearAcceleration + " / " + yLinearAcceleration + " / " + zLinearAcceleration);
+		}
 	}
 
 }
