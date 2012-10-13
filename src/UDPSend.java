4414e6da3125de3eec87bd1d2be830f62f7af55c
Evan Ye <applepie31415@gmail.com>
2012-10-13 04:24:15 -0700

    updated UDPsend to send to evan's machine

--- src/UDPSend.java
+++ src/UDPSend.java
@@ -5,32 +5,21 @@ import java.net.InetAddress;
 
 public class UDPSend {
 
-	private static String ip_address = "192.168.135.1";
-	private static int port = 9009;
+	private static String ip_address = "169.254.14.183";
+	private static int port = 9002;
 	
-    public static void sendUDPMessage(String message) throws java.io.IOException {
+	public static void send(double x, double y, boolean release) throws IOException{
+		String releaseString = (release) ? "True" : "False";
+		sendUDPMessage("[" + x + "," + y + "," + releaseString +"]");
+	}
+	
+    public static void sendUDPMessage(String message) throws IOException {
         DatagramSocket socket = new DatagramSocket();
         InetAddress serverIP = InetAddress.getByName(ip_address);
-        System.out.println(serverIP);
         byte[] outData = (message).getBytes();
+        System.out.println(serverIP + ": " + message);
         DatagramPacket out = new DatagramPacket(outData,outData.length, serverIP, port);
         socket.send(out);
         socket.close();
     }
-    
-    public static void main(String[] args){
-    	System.out.println("Asdf" + 1);
-    	try {
-    		for(int i = 0; i < 100; i++){
-    			sendUDPMessage("Pinging" + i + 1);
-    			Thread.sleep(1000);
-    		}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-    	System.out.println("Asdfa");
-    }
-
 }
