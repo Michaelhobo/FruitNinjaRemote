2290bce2dba467f98a6597fe50a921db0c1b0f07
Michael Ho <callmemikeh@gmail.com>
2012-10-12 22:15:50 -0700

    initial commit 2

--- src/AccelerometerDataActivity.java
+++ src/AccelerometerDataActivity.java
@@ -1,4 +1,24 @@
+import com.fruitninjaremote.R;
 
-public class AccelerometerDataActivity {
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AccelerometerDataActivity extends Activity implements
+		SensorEventListener {
+	int mLastX;
+	int mLastY;
+	int mLastZ;
+	boolean mInitialized;
+
+	public void onAccuracyChanged(Sensor arg0, int arg1) {	}
+
+	public void onSensorChanged(SensorEvent event) 
+	{
+		
+	}
 
 }
